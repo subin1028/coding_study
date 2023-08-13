72b02394bca4f5466d4949815875da964828dac4
subbie <2lsbapple@gmail.com>
2023-08-14 02:12:58 +0900

    v3

--- PrimeNumber/prime_num.py
+++ PrimeNumber/prime_num.py
@@ -10,13 +10,16 @@ def check_prime(num):
 
 def solution(numbers):
     answer = 0
+    permu_list = []
     li_num = list(numbers)
-    permu_list = list(permutations(li_num, 1))
-    permu_list += list(permutations(li_num))
+    
+    for i in range(1, len(numbers)+1):
+        permu_list += list(permutations(li_num, i))
+        
     f_nums = list(set([int(''.join(num)) for num in permu_list]))
     
     for i in f_nums:
         if(check_prime(i)):
             answer += 1
-    
+
     return answer
