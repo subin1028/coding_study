eaacf81fe5b258fe97d8d5ca054209273aced729
subbie <2lsbapple@gmail.com>
2023-08-14 19:33:36 +0900

    v1

--- Carpet/carpet.py
+++ Carpet/carpet.py
@@ -1,3 +1,16 @@
 def solution(brown, yellow):
+    total = brown + yellow
+    divisor = []
     answer = []
+    for i in range(1, total+1):
+        if total % i == 0:
+            divisor.append(i)
+    length = len(divisor)
+            
+    if length % 2 == 0:
+        answer = [divisor[length//2], divisor[length//2 - 1]]
+        
+    else:
+        answer = [divisor[length//2]] * 2
+        
     return answer
