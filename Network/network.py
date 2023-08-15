22ea19a4375fdd1e6de848fb5144ea92ef3f1d38
subbie <2lsbapple@gmail.com>
2023-08-16 02:29:36 +0900

    v1

--- Network/network.py
+++ Network/network.py
@@ -1,3 +1,31 @@
+def count_tower(n, num, wires, select):
+    oth_num = []
+    cnt_num = 1
+    
+    for i in wires:
+        if i != select:
+            if num in i:
+                cnt_num += 1
+                oth_num += [oth for oth in i if (num in i)&(oth!=num)]
+            
+    for i in wires:
+        if i != select:            
+            for j in oth_num:
+                if (j in i) and ((num in i) == False):
+                    cnt_num += 1
+
+    cha = abs((n - cnt_num) - (cnt_num))
+    return cha   
+    
+
 def solution(n, wires):
-    answer = -1
+    count = []
+    
+    for i in wires:
+        select = i
+        num = i[0]
+        ans = count_tower(n, num, wires, select)
+        count.append(ans)
+    
+    answer = min(count)
     return answer
