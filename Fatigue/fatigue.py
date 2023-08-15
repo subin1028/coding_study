93790bbf635b2bb433ef45ee0e273c6bc5264cd4
subbie <2lsbapple@gmail.com>
2023-08-15 12:59:54 +0900

    v2

--- Fatigue/fatigue.py
+++ Fatigue/fatigue.py
@@ -3,18 +3,24 @@ from itertools import permutations
 def solution(k, dungeons):
     answer = 0
     fatigue = k
-    route = list(permutations(dungeons))
+    times = []
+    route = list(permutations(dungeons)) # 가능한 경로 생성
     
     for i in route:
-        if answer == len(dungeons):
-            break
-        
         fatigue = k
+        if answer == len(dungeons): #던전 개수와 횟수가 같을 경우
+            break #종료
+        
         answer = 0
         
         for j in i:
-            if fatigue >= j[0]:
+            if fatigue >= j[0]: 
                 fatigue -= j[1] 
                 answer += 1
-
+                
+            else:
+                break
+        times.append(answer)
+        
+    answer = max(times) #나온 결과 중 가장 큰 값 반환
     return answer
