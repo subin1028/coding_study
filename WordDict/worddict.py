719a7588757c553672f80960fdaff5827d60ca59
subbie <2lsbapple@gmail.com>
2023-08-15 14:22:45 +0900

    v1

--- WordDict/worddict.py
+++ WordDict/worddict.py
@@ -1,3 +1,15 @@
+from itertools import product
 def solution(word):
+    ex = ['A', 'E', 'I', 'O', 'U']
+    w_dict = []
+    
+    for i in range(1, 6):
+        for j in product(ex, repeat = i):
+            w_dict.append(''.join(j))
+            
+    w_dict.sort()
+    answer = w_dict.index(word) + 1
+
+    print(answer)
     answer = 0
     return answer
