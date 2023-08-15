41733cac06954813280fc82b89e9e8ae181f7db8
subbie <2lsbapple@gmail.com>
2023-08-15 12:48:14 +0900

    v1

--- Fatigue/fatigue.py
+++ Fatigue/fatigue.py
@@ -1,3 +1,20 @@
+from itertools import permutations
+
 def solution(k, dungeons):
-    answer = -1
+    answer = 0
+    fatigue = k
+    route = list(permutations(dungeons))
+    
+    for i in route:
+        if answer == len(dungeons):
+            break
+        
+        fatigue = k
+        answer = 0
+        
+        for j in i:
+            if fatigue >= j[0]:
+                fatigue -= j[1] 
+                answer += 1
+
     return answer
