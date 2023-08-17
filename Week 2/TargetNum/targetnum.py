fe578c3946265266a98774359d42abc6bc6981de
subbie <2lsbapple@gmail.com>
2023-08-17 13:54:51 +0900

    v1

--- Week 2/TargetNum/targetnum.py
+++ Week 2/TargetNum/targetnum.py
@@ -1,3 +1,27 @@
+from itertools import permutations, product
+def make_plma(n):
+    plma = ['+', '-']
+    plma = list(product(plma, repeat = n))
+    print(plma)
+    return plma
+
+def calculate(plma, nums):
+    pass
+                
+
 def solution(numbers, target):
     answer = 0
+    nums = list(set(permutations(numbers)))
+    plma = make_plma(len(numbers))
+    for a in range(0, len(plma)):
+        for i,j in zip(nums, plma[a]):
+            print(i,j)
+    
     return answer
+
+numbers = [1, 1, 1, 1, 1]
+target = 3
+
+solution(numbers, target)
+#숫자와 기호의 모든 조합을 엮어서 계산하려고 함
+#시간복잡도 최악
