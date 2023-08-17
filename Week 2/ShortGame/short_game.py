9aa1c73c7055fcd281378ace749cc2f3bf9015da
subbie <2lsbapple@gmail.com>
2023-08-17 19:41:58 +0900

    v2

--- Week 2/ShortGame/short_game.py
+++ Week 2/ShortGame/short_game.py
@@ -1,35 +1,49 @@
+from collections import deque
 def solution(maps):
     answer = 0
-    n = len(maps) #맵의 세로 길이
-    m = len(maps[0]) #맵의 가로 길이
+    n = len(maps) #맵 세로 길이
+    m = len(maps[0]) #맵 가로 길이
 
-    graph = []
-    for i in range(n):
-        graph.append(list(map(int, input())))
-        
-    result = 0
-    for i in range(n):
-        for j in range(m):
-            if dfs(i, j) == True:
-                result += 1
-                
-    print(result)
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1] #위, 아래, 왼, 오
+    
+    print(bfs(maps, 0,0, dx, dy, n, m))
+    
     return answer
 
-def dfs(x,y,graph):
-    if x <= -1 or x > = n or y <= -1 or y > = m:
-        return False
-    if graph[x][y] == 0:
-        graph[x][y] = 1
+def bfs(graph, x, y, dx, dy, n, m):
+    count = 0
+    route = [[0,0]]
+    queue = deque()
+    queue.append((x, y))
+    
+    while queue:
+        x, y = queue.popleft()
         
-        dfs(x-1, y)
-        dfs(x, y-1)
-        dfs(x+1, y)
-        dfs(x, y+1)
-        return True
-    return False
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if ([nx, ny] != route[count-1]):
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                
+                if graph[nx][ny] == 0:
+                    continue
+                
+                if (graph[nx][ny] == 1):
+                    route.append([nx, ny])
+                    count += 1
+                    queue.append((nx, ny))
+                    break
+                    
+            else:
+                continue
+                
+    return count, route
+    
 
 maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
 
 solution(maps)
-#bfs로 전환
+# 한가지의 루트밖에 찾아내지 못함
