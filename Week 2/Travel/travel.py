da5008c8c45276a1fe2eef0e30e1b15d9a7d1b29
subbie <2lsbapple@gmail.com>
2023-08-19 01:24:29 +0900

    v2

--- Week 2/Travel/travel.py
+++ Week 2/Travel/travel.py
@@ -4,57 +4,52 @@ def mapping(tickets): #항공권을 숫자로 매핑하는 함수
     ticket_dict = {}
     orient = list(set(sum(tickets, []))) #요소 추출
     orient.sort() #알파벳순으로 오도록 정렬
-    orient.remove("ICN")
-    orient.insert(0, "ICN") #ICN이 맨 앞으로 오도록 조정 
     for i, ticket in enumerate(orient):
         ticket_dict[ticket] = i #딕셔너리 생성
         
     mapp = [[ticket_dict[start], ticket_dict[des]] for start, des in tickets]
     #항공권을 숫자로 매핑
     
-    print(ticket_dict)
     return mapp, len(orient), ticket_dict
 
-def bfs(graph, start):
-    queue = deque()
-    ans = []
+def dfs(graph, current, len_tickets, visited, path):
+    path.append(current)
     
-    queue.append(start)
+    if len(path) == len_tickets + 1:
+        return True
     
-    while queue:
-        current = queue.popleft()
-        print(current, end= ' ')
-        ans.append(current)
-        
-        for i in graph[current]:
-            queue.append(i)
-            graph[current].remove(i)
-            
-    return ans
+    for next_city in graph[current]:
+        if not visited[current][next_city]:
+            visited[current][next_city] = True
+            if dfs(graph, next_city, len_tickets, visited, path):
+                return True
+            visited[current][next_city] = False #백트래킹을 위해 제자리
     
+    path.pop()
+    return False
+
 def solution(tickets):
     answer = []
     mapp, n, ticket_dict = mapping(tickets)
     graph = [[] for _ in range(n)]
     
     for i,j in mapp:
-        graph[i] += [j] #그래프 생성
+        graph[i].append(j) #그래프 생성
     
     for i in graph:
-        if 0 in i:
-            pass
-        
-        else:
-            i.sort()
-        
-    print(graph)
-    pre_ans = bfs(graph, 0)
-  
-    answer = [code for ans in pre_ans for code, num in ticket_dict.items() if num == ans]
+        i.sort()
     
-    print(answer)
+    visited = [[False] * n for _ in range(n)]
+    start_city = ticket_dict["ICN"]
+    path = []
+    
+    dfs(graph, start_city, len(tickets), visited, path)
+    
+    answer = [list(ticket_dict.keys())[list(ticket_dict.values()).index(city)] for city in path]
+    
+    
     return answer
 
-tickets = [["ICN", "SFO"], ["SFO", "ICN"], ["ICN", "SFO"], ["SFO", "QRE"]]
 
+tickets = [["ICN", "B"], ["B", "ICN"], ["ICN", "A"], ["A", "D"], ["D", "A"]]
 solution(tickets)
