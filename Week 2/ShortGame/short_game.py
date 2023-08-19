f46a7429ac4cc3e0885df3b280e4eb494724b5f1
subbie <2lsbapple@gmail.com>
2023-08-19 16:23:52 +0900

    v4

--- Week 2/ShortGame/short_game.py
+++ Week 2/ShortGame/short_game.py
@@ -1,44 +1,34 @@
 from collections import deque
 def solution(maps):
-    answer = 0
+
     n = len(maps) #맵 세로 길이
     m = len(maps[0]) #맵 가로 길이
 
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1] #위, 아래, 왼, 오
     
-    answer = bfs(maps, 0,0, dx, dy, n, m)
-    print(answer)
-    
-    return answer
+    check = [[-1 for _ in range(m)] for _ in range(n)]
+   
 
-def bfs(graph, x, y, dx, dy, n, m):
-    route = [[0,0]]
     queue = deque()
-    queue.append((x, y)) 
+    queue.append([0, 0]) 
+    
+    check[0][0] = 1
     
     while queue:
-        x, y = queue.popleft()
-        
-        for i in range(4):
+       y,x = queue.popleft()
+       
+       for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if 0 <= nx < n and 0 <= ny < n:
+            if 0 <= ny < n and 0 <= nx < m and (maps[ny][nx] == 1):
 
-                if (graph[nx][ny] == 1):
-                    route.append([nx, ny])
-                    graph[nx][ny] = graph[x][y] + 1
-                    graph[x][y] = -1
-                    queue.append((nx, ny))
-                        
-                if (nx == (n-1)) and (ny == (m-1)):
-                    print(graph)
-                    return max(map(max, graph))
+                if check[ny][nx] == -1:
+                    check[ny][nx] = check[y][x] + 1
+                    queue.append([ny, nx])
    
-    if graph[n-1][m-1] == 1: 
-        return -1
-    return max(map(max, graph))
+    return check[-1][-1]
  
-maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]
+maps = [[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]
 solution(maps)
