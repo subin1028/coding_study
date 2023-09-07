3162a20114035b11cb98290eca8bfb73e24efe2b
subbie <2lsbapple@gmail.com>
2023-09-07 19:18:51 +0900

    v1.0.0

--- Week 5/training.py
+++ Week 5/training.py
@@ -3,30 +3,22 @@ def solution(n, lost, reserve):
         if r in lost:
             lost.remove(r)
             reserve.remove(r)
+    lost.sort()
     
-    have = n - len(lost)
-    
-    for i in lost:
-        if i in reserve:
-            reserve.remove(i)
-            continue
-        
-        if i+1 in reserve:
-            have += 1
-            reserve.remove(i+1)
-            continue
-        
-        elif i-1 in reserve:
-            have += 1
+    for i in lost[:]:
+        if i-1 in reserve: #1더한 값이 있을 경우
             reserve.remove(i-1)
-            continue
+            lost.remove(i)
 
-            
-    print(have)
-    return have
+        elif i+1 in reserve:
+            reserve.remove(i+1)
+            lost.remove(i)
 
-n = 1
-lost = [2]
-reserve = [2]
+    print(n-len(lost))
+    return n - len(lost)
+
+n = 5
+lost = [2,4]
+reserve = [1,3]
 
 solution(n, lost, reserve)
