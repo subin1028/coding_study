70eb70bb6f90855ca48b606d8fbdad00573709cb
subbie <2lsbapple@gmail.com>
2023-09-22 23:49:33 +0900

    v1.0.0

--- Week 6/Hash/phonelist.py
+++ Week 6/Hash/phonelist.py
@@ -6,16 +6,10 @@ def solution(phone_book):
     
     phone_book.sort()
     
-    #한 번 true인 애들이 또 검사된다
     for i in range(0, len(phone_book)-1):
-        for j in range(i+1, len(phone_book)):
-            if phone_book[i][0] != phone_book[j][0]:
-                    break
-            else:
-                temp = phone_book[i].replace(phone_book[j], "a")
-                temp1 = phone_book[j].replace(phone_book[i], "a")
-                if temp[0] == "a" or temp1[0] == "a":
-                    return False               
+        if len(phone_book[i]) < len(phone_book[i+1]):
+            if phone_book[i+1][:len(phone_book[i])] == phone_book[i]: 
+                return False               
     return answer
 
 phone_book = ["12", "123", "1235", "567", "88"]
