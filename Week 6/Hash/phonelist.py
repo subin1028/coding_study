1f20e9247105cc701b67e666eb7cc686e3aec4c7
subbie <2lsbapple@gmail.com>
2023-09-22 21:34:25 +0900

    vf2

--- Week 6/Hash/phonelist.py
+++ Week 6/Hash/phonelist.py
@@ -6,13 +6,15 @@ def solution(phone_book):
     if len(phone_book) == 1:
         return True
     
-    for i in range(0, len(phone_book)-1):
-        for j in range(i+1, len(phone_book)):
-            temp = phone_book[j].replace(phone_book[i], "a")
-            if temp[0] == "a":
-                print("false")
-                return False
-    print("true") 
+    for i in phone_book:
+        for j in phone_book:
+            if i == j: continue
+            else:
+                if j[0] == i[0]:
+                    temp = i.replace(j, "a")
+                    temp1 = j.replace(i, "a")
+                    if temp[0] == "a" or temp1[0] == "a":
+                        return False
     return answer
 
 phone_book = ["12", "123", "1235", "567", "88"]
