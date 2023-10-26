ccd8081c8e63d6257f6a849bb9d5b1ac75743a26
subbie <2lsbapple@gmail.com>
2023-10-26 09:55:43 +0900

    start

---
+++ Week 11/14891.py
@@ -0,0 +1,18 @@
+
+wheel = []
+for _ in range(4):
+    wheel.append(list(input().split()))
+
+time = int(input())
+times = []
+for _ in range(time):
+    times.append(list(input().split(" ")))
+# 10101111
+# 01111101
+# 11001110
+# 00000010
+# 2
+# 3 -1
+# 1 1
+
+# 6번째
