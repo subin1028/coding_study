7144c7fa30b9d83d695d1ea4dc4625519cb241e1
subbie <2lsbapple@gmail.com>
2023-11-05 20:14:11 +0900

    f2

--- Week 12/11000.py
+++ Week 12/11000.py
@@ -1,19 +1,27 @@
+from collections import deque
 N = int(input())
 lecture = [list(map(int, input().split())) for _ in range(N)]
 time = [False for _ in range(N)]
 cs = 0
-
 #[[1, 3], [2, 4], [3, 5]]
 lecture.sort()
-for i in range(0, N-1):
-    for j in range(i+1, N):
-        if lecture[j][0] >= lecture[i][1] and time[j] == False:
-            time[i] = True
-            time[j] = True
-            cs += 1 
-            break
-for t in time:
-    if t == False:
-        cs += 1
+q = deque(lecture)
+while q:
+    _, t1 = q.popleft()
+    for i in range(len(q)):
+        t2, t3 = q.popleft()
+    
+        if t1 <= t2:
+            cs += 1
+            if len(q) == 1:
+                cs += 1
+                break
+            continue
         
+        else:
+            q.append([t2, t3])
+            print(len(q))
+            if len(q) == 1:
+                cs += 1
+                break
 print(cs)
