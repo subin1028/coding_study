f4374a672ec77a23324a424618bec0da24e6b24e
subbie <2lsbapple@gmail.com>
2023-11-05 18:40:34 +0900

    v1.0.0

--- Week 12/15686.py
+++ Week 12/15686.py
@@ -19,27 +19,22 @@ for i in range(N):
 combi_chicken = deque(list(combinations(chicken, M)))
 wh_dis = 0     
 max_dis = N * 2     
-if len(chicken) == M:
-    for r,c in home:
+
+total = 1e9
+
+while combi_chicken:
+    wh_dis = 0
+    chickens = combi_chicken.popleft()
+    for r,c in home: #집 돌아
         max_dis = N * 2 
-        for cr, cc in chicken:
+        for cr, cc in chickens: #집마다 치킨 집 거리 계산해
             dis = abs(r-cr) + abs(c-cc)
-            max_dis = min(max_dis, dis)
-            
-        wh_dis += max_dis
+            max_dis = min(max_dis, dis) #제일 작은 거리 구해     
+            # print(f'{(r,c)}번째 집 {cr, cc}번째 치킨집: 최소: {max_dis}, 그냥 거리: {dis}')      
         
-else:
-    total = N * 2 * M
-    while combi_chicken:
-        chickens = combi_chicken.popleft()
-        for r,c in home:
-            max_dis = N * 2 
-            for cr, cc in chickens:
-                dis = abs(r-cr) + abs(c-cc)
-                max_dis = min(max_dis, dis)            
-            
-            wh_dis += max_dis
-        total = min(total, wh_dis)
-              
-print(wh_dis)
+        wh_dis += max_dis #거리 합 구해
+        # print("최소 거리: ", wh_dis)
+    total = min(total, wh_dis) #합 중에 최소 구해
+    # print("최소 거리 합: ", total)
+
 print(total)
