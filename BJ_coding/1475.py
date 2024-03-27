7de2abe0146f167119818e94262e65f84e4fbb89
subin1028 <2lsbapple@gmail.com>
2024-03-27 16:57:57 +0900

    v2

--- BJ_coding/1475.py
+++ BJ_coding/1475.py
@@ -1,20 +1,9 @@
-nums = [1] * 10
-sets = 1
+nums = [0] * 10
+ans = 0
 room = list(map(int, input()))
 
-for n in room:
-    if nums[n] > 0: 
-        nums[n] -= 1
+for i in room:
+    nums[i] += 1
 
-    elif n == 9 and nums[6] > 0:
-        nums[6] -= 1
-
-    elif n == 6 and nums[9] > 0:
-        nums[9] -= 1
-
-    else:
-        sets += 1
-        nums = [x+1 for x in nums]
-        nums[n] = 0
-
-print(sets)
+ans = max(max([x for i, x in enumerate(nums) if i != 6 and i != 9]), (nums[6] + nums[9] + 1) // 2)
+print(ans)
